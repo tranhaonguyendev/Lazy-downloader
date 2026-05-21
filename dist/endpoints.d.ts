a95eda43cb4585a37ac43c458fecae9372d128f0
eDev <haonguyen2100hn@gmail.com>
2026-05-21 17:33:29 +0700

    Fast up

--- dist/endpoints.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const BASE = "https://j2download.com";
-export declare const Endpoints: {
-    home: () => string;
-    api: () => string;
-    autolink: () => string;
-    authBootstrap: () => string;
-    authIssue: () => string;
-};
--- dist/endpoints.js
+++
@@ -1,8 +0,0 @@
-export const BASE = "https://j2download.com";
-export const Endpoints = {
-    home: () => `${BASE}/`,
-    api: () => `${BASE}/api`,
-    autolink: () => `${BASE}/api/autolink`,
-    authBootstrap: () => `${BASE}/api/auth/bootstrap`,
-    authIssue: () => `${BASE}/api/auth/issue`
-};
